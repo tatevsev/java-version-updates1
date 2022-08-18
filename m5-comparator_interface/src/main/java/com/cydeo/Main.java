2e377adeed6fad10760ddeacae9728b5cfaec61e
ozzy <ozzy@cydeo.com>
2022-08-18 13:08:42 -0400

    Double Colon Operator

---
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,25 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10,0,15,5,20);
+        System.out.println(list);
+
+        //Ascending
+        Collections.sort(list);
+        System.out.println(list);
+
+        //Descending
+        Collections.sort(list,new MyComparator());
+        System.out.println(list);
+
+        Collections.sort(list,((o1,o2) -> (o1>o2) ? -1 : (o1<o2) ? 1 : 0));
+
+    }
+}
