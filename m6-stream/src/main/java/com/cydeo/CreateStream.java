bbef7095c04b7f42f70c0f4ac9e37daac05c1c71
ozzy <ozzy@cydeo.com>
2022-08-19 11:59:28 -0400

    Stream Operations

--- m6-stream/src/main/java/com/cydeo/CreateStream.java
+++ m6-stream/src/main/java/com/cydeo/CreateStream.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Main {
+public class CreateStream {
     public static void main(String[] args) {
 
         //Creating Stream from Array
@@ -23,6 +23,11 @@ public class Main {
         );
 
         Stream<Course> myCourseStream = myCourses.stream();
+
+        //Creating Stream Values
+        Stream<Integer> stream = Stream.of(1,2,3,4);
+
+
 
 
 
